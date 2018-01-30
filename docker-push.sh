26e1612b3c6845b6f3ef605cbb9ca1fe56cf5697
Michael Herman <hermanmu@gmail.com>
2018-01-30 08:16:43 -0700

    added ecr into the ci process (part 5)

---
+++ docker-push.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]
+then
+
+  if [ "$TRAVIS_BRANCH" == "staging" ] || \
+     [ "$TRAVIS_BRANCH" == "production" ]
+  then
+    curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+    unzip awscli-bundle.zip
+    ./awscli-bundle/install -b ~/bin/aws
+    export PATH=~/bin:$PATH
+    # add AWS_ACCOUNT_ID, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY env vars
+    eval $(aws ecr get-login --region us-east-1 --no-include-email)
+    export TAG=$TRAVIS_BRANCH
+    export REPO=$AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com
+  fi
+
+  if [ "$TRAVIS_BRANCH" == "staging" ] || \
+     [ "$TRAVIS_BRANCH" == "production" ]
+  then
+    # users
+    docker build $USERS_REPO -t $USERS:$COMMIT -f Dockerfile-prod
+    docker tag $USERS:$COMMIT $REPO/$USERS:$TAG
+    docker push $REPO/$USERS:$TAG
+    # users db
+    docker build $USERS_DB_REPO -t $USERS_DB:$COMMIT -f Dockerfile
+    docker tag $USERS_DB:$COMMIT $REPO/$USERS_DB:$TAG
+    docker push $REPO/$USERS_DB:$TAG
+    # client
+    docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-prod --build-arg REACT_APP_USERS_SERVICE_URL=TBD
+    docker tag $CLIENT:$COMMIT $REPO/$CLIENT:$TAG
+    docker push $REPO/$CLIENT:$TAG
+    # swagger
+    docker build $SWAGGER_REPO -t $SWAGGER:$COMMIT -f Dockerfile-prod $SWAGGER_DIR
+    docker tag $SWAGGER:$COMMIT $REPO/$SWAGGER:$TAG
+  fi
+fi
