9d712620b8eccf728ee663db0b26b997c5516a37
Michael Herman <hermanmu@gmail.com>
2018-02-07 17:24:16 -0700

    part 6 (wip)

--- docker-deploy-prod.sh
+++ docker-deploy-prod.sh
@@ -62,6 +62,14 @@ then
       register_definition
       update_service
 
+      # exercises
+      service="testdriven-exercises-prod-service"
+      template="ecs_exercises_prod_taskdefinition.json"
+      task_template=$(cat "ecs/$template")
+      task_def=$(printf "$task_template" $AWS_ACCOUNT_ID "tbd")
+      echo "$task_def"
+      register_definition
+
     }
 
     configure_aws_cli
