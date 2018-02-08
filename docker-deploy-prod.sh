62e120de45927904827f1cddc2a6f3ed273f22c6
Michael Herman <hermanmu@gmail.com>
2018-02-07 19:30:06 -0700

    part 6 (wip)

--- docker-deploy-prod.sh
+++ docker-deploy-prod.sh
@@ -66,7 +66,7 @@ then
       service="testdriven-exercises-prod-service"
       template="ecs_exercises_prod_taskdefinition.json"
       task_template=$(cat "ecs/$template")
-      task_def=$(printf "$task_template" $AWS_ACCOUNT_ID "tbd")
+      task_def=$(printf "$task_template" $AWS_ACCOUNT_ID $AWS_RDS_EXERCISES_URI)
       echo "$task_def"
       register_definition
 
